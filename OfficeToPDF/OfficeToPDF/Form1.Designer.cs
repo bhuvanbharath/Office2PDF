146f914025431468e2189847f288f3dbf577d525
bhuvanbharath <bhuvanbharath@outlook.com>
2023-08-17 19:06:14 +0530

    initial commit

---
+++ OfficeToPDF/OfficeToPDF/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace OfficeToPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 123);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Word";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(13, 148);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 19);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "PowerPoint";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(13, 173);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(53, 19);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Excel";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Convert Office documents to PDF files with a single click!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 223);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OfficeToPDF/OfficeToPDF/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace OfficeToPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                textBox1.Text = selectedFolderPath; // Update the textBox1 with the selected folder path
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string folderPath = textBox1.Text;
+
+            if (Directory.Exists(folderPath))
+            {
+                ConvertDocumentsToPDF(folderPath);
+                MessageBox.Show("Conversion completed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid folder path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConvertDocumentsToPDF(string folderPath)
+        {
+            string[] documentExtensions = { ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx" };
+
+            foreach (string filePath in Directory.GetFiles(folderPath))
+            {
+                //MessageBox.Show(filePath, "name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                string fileExtension = Path.GetExtension(filePath).ToLower();
+                //MessageBox.Show(fileExtension, "extension", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (Array.Exists(documentExtensions, ext => ext == fileExtension))
+                {
+                    string pdfFilePath = Path.ChangeExtension(filePath, ".pdf");
+
+                    if (File.Exists(pdfFilePath))
+                    {
+                        File.Delete(pdfFilePath);
+                    }
+                    //MessageBox.Show(pdfFilePath, "pdf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    object application = null;
+
+                    try
+                    {
+                        if (fileExtension == ".doc" || fileExtension == ".docx")
+                        {
+                            //MessageBox.Show("doc Entry", "name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            application = new Microsoft.Office.Interop.Word.Application();
+                            Document document = ((Microsoft.Office.Interop.Word.Application)application).Documents.Open(filePath);
+                            document.ExportAsFixedFormat(pdfFilePath, WdExportFormat.wdExportFormatPDF);
+                            document.Close();
+                        }
+                        else if (fileExtension == ".xls" || fileExtension == ".xlsx")
+                        {
+                            //MessageBox.Show("excel Entry", "name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            application = new Microsoft.Office.Interop.Excel.Application();
+                            Workbook workbook = ((Microsoft.Office.Interop.Excel.Application)application).Workbooks.Open(filePath);
+                            workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, pdfFilePath);
+                            workbook.Close();
+                        }
+                        else if (fileExtension == ".ppt" || fileExtension == ".pptx")
+                        {
+                            MessageBox.Show("ppt Entry", "name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            application = new Microsoft.Office.Interop.PowerPoint.Application();
+                            Presentation presentation = ((Microsoft.Office.Interop.PowerPoint.Application)application).Presentations.Open(filePath);
+                            presentation.ExportAsFixedFormat(pdfFilePath, PpFixedFormatType.ppFixedFormatTypePDF);
+                            presentation.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error converting {Path.GetFileName(filePath)}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        if (application != null)
+                        {
+                            application.GetType().InvokeMember("Quit", System.Reflection.BindingFlags.InvokeMethod, null, application, null);
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
